8aa8e4d4fcb4d5c53fe5c6aa21c9e6c73d92f65c
devalmazbek <devalmazbek@gmail.com>
2024-06-19 11:43:13 +0600

    added state by redux devtools

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -7,12 +7,15 @@ const initialState= {
 	isLoading: false,
 };
 
+const host = 'api.frankfurter.app';
+
 const accountSlice = createSlice({
 	name: 'account',
 	initialState,
 	reducers: {
 		deposit(state, action) {
 			state.balance += action.payload;
+			state.isLoading = false;
 		},
 
 		widthdraw(state, action) {
@@ -39,16 +42,31 @@ const accountSlice = createSlice({
 			state.balance -= state.loan;
 			state.loan = 0;
 			state.purpose = "";
+		},
+		
+		loading( state ) {
+			state.isLoading = true;
 		}
 	}
 });
 
-export const { deposit, widthdraw, requestLoan, payLoan } = accountSlice.actions;
+export const { widthdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+	if(currency === 'USD') {
+		return { type: "account/deposit", payload: amount }
+	}
+
+	return async function(dispatch, getState) {
+		dispatch({ type: "account/loading" })
+		
+		const request = await fetch(`https://${host}/latest?amount=${amount}&from=${currency}&to=USD`);
+		const data = await request.json();
+		const converted = data.rates.USD;
+
+		dispatch({ type: "account/deposit", payload: converted })
+	}
+	
+}
 
 export default accountSlice.reducer;
-
-
-
-
-
-
--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,36 +1,35 @@
-const ininitialStoreCustomer = {
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
 	fullname: '',
 	nationalID: '',
 	createdAt: '',
 }
 
-export default function reducerCustomer(state = ininitialStoreCustomer, action) {
-	switch(action.type) {
-		case "customer/createCustomer": 
-			return {...state, 
-				fullname: action.payload.fullname,
-				nationalID: action.payload.nationalID,
-				createdAt: action.payload.createdAt,
-			};
+const customerSlice = createSlice({
+	name: 'customer',
+	initialState,
+	reducers: {
+		createCustomer: {
+			prepare(fullname, nationalID) {
+				return {
+					payload: { fullname, nationalID, createdAt: new Date().toISOString() }
+				};
+			},
+			
+			reducer(state, action) {
+				state.fullname = action.payload.fullname;
+				state.nationalID = action.payload.nationalID;
+				state.createdAt = action.payload.createdAt;
+			}
+		},
 
-		case "customer/updateName": 
-			return { ...state, fullname: action.payload };
-
-		default: 
-			return state
+		updateName(state, action) {
+			state.fullname = action.payload.fullname;
+		}
 	}
-}
+});
 
-export function createCustomer(fullname, nationalID) {
-	return {
-		type: "customer/createCustomer",
-		payload: { fullname, nationalID, createdAt: new Date().toISOString() },
-	}
-}
+export const { createCustomer, updateName } = customerSlice.actions;
 
-export function updateName(fullname) {
-	return {
-		type: "customer/updateName",
-		payload: fullname,
-	}
-}
+export default customerSlice.reducer;
